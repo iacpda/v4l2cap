e5bdec69fc0182ae55ce79f1218ec1dfe90403ea
Israel Cepeda <israel.a.cepeda.lopez@intel.com>
2015-12-23 13:34:44 -0800

    v4l2cap: open default video device
    Open default video device video0, but still doing nothing with it. Just
    to verify it is able to open it and return the fd.
    
    Signed-off-by: Israel Cepeda <israel.a.cepeda.lopez@intel.com>

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define VIDEODEV "/dev/video0"
 
 int main(int argc, char **argv)
 {
+	int fd;
+
+	fd = open(VIDEODEV, O_RDWR);
+	if (fd <= 0) {
+		printf("ERROR: Opening %s\n", VIDEODEV);
+		return 1;
+	}
+
+	/* Notify we are not functional yet */
 	printf("Sorry, still WIP\n");
 
+	close(fd);
 	return 0;
 }
